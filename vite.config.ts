f9627c815a6b0f03d64b36ec0264af39639ea6ea
Pratik Jatale <jitcse.pratikjatale11@gmail.com>
2026-06-15 00:02:40 +0530

    refactor: update dev server proxy targets and apply formatting changes in vite config

--- vite.config.ts
+++ vite.config.ts
@@ -1,25 +1,33 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
-import { fileURLToPath, URL } from 'node:url'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
+import { fileURLToPath, URL } from "node:url";
 
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
   resolve: {
     alias: {
-      '@': fileURLToPath(new URL('./src', import.meta.url)),
+      "@": fileURLToPath(new URL("./src", import.meta.url)),
     },
   },
   server: {
     proxy: {
-      '/upload': {
-        target: 'http://72.62.227.125:3002',
+      // "/upload": {
+      //   target: "http://72.62.227.125:3002",
+      //   changeOrigin: true,
+      // },
+      // "/uploads": {
+      //   target: "http://72.62.227.125:3002",
+      //   changeOrigin: true,
+
+          "/upload": {
+        target: "https://marbellasociety.4tech.in/api",
         changeOrigin: true,
       },
-      '/uploads': {
-        target: 'http://72.62.227.125:3002',
+      "/uploads": {
+        target: "https://marbellasociety.4tech.in/api",
         changeOrigin: true,
       },
     },
   },
-})
+});
